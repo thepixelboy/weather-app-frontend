531705f542e7948623a79a795d6ee242029bca5e
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-12-20 13:05:42 +0100

    Browser HTTP Requests with Fetch

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,17 @@
 console.log('Cliente side javascript file loaded...');
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+  response.json().then((data) => {
+    console.log(data);
+  });
+});
+
+fetch('/weather/?address=boston').then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data[0].location, data[0].forecast);
+    }
+  });
+});
