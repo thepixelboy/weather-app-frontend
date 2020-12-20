d64b332bab5b381e999ea6c1d8a2ad8493c23501
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-12-20 18:06:28 +0100

    Wiring up the User Interface

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,23 @@
 const weatherForm = document.querySelector('form');
 const searchElement = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
   const location = searchElement.value;
 
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+
   fetch(`/weather/?address=${location}`).then((response) => {
     response.json().then((data) => {
       if (data.error) {
-        console.log(data.error);
+        messageOne.textContent = data.error;
       } else {
-        console.log(data[0].location);
-        console.log(data[0].forecast);
+        messageOne.textContent = data[0].location;
+        messageTwo.textContent = data[0].forecast;
       }
     });
   });
